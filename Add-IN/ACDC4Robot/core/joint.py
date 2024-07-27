1c40142f7c06658b1b572793571320f344e33d92
nuofan <12233200@mail.sustech.edu.cn>
2024-07-27 23:01:15 +0800

    update for joint name convension to make it follows WYSWYG principle

--- Add-IN/ACDC4Robot/core/joint.py
+++ Add-IN/ACDC4Robot/core/joint.py
@@ -31,8 +31,10 @@ class Joint():
         """
         # joint names inside each occurrence are identical
         # but joint names in different occurrences can be the same, in which makes conflict
-        parent_name = utils.get_valid_filename(self.parent.fullPathName) # inorder to fix joint's name confliction
-        name = parent_name + "_" + utils.get_valid_filename(self.name)
+        # parent_name = utils.get_valid_filename(self.parent.fullPathName) # inorder to fix joint's name confliction
+        # name = parent_name + "_" + utils.get_valid_filename(self.name)
+        # output name is consistent with the name in Fusion
+        name = utils.get_valid_filename(self.name)
         return name
 
     def get_parent(self):
